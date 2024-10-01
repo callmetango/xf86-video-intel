6e4d0b5f4573172a2bce1d6fa4a7dfabe75adceb
Enrico Weigelt, metux IT consult <info@metux.net>
2024-10-01 18:17:47 +0200

    HACK: use mtx branch due regression in mainline

--- .gitlab-ci/common/build-xserver.sh
+++ .gitlab-ci/common/build-xserver.sh
@@ -13,7 +13,7 @@ fi
 
 XSERVER_CLONE=/tmp/xserver
 XSERVER_BUILD=$XSERVER_CLONE/_builddir
-XSERVER_REPO=https://gitlab.freedesktop.org/xorg/xserver.git
+XSERVER_REPO=https://gitlab.freedesktop.org/metux/xserver.git
 
 MACH=`gcc -dumpmachine`
 
